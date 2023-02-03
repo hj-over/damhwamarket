e0894b1b944bdb798a85920bd6e67ce9388a7d16
hj.over <dhfldhs0707@gmail.com>
2023-02-03 18:54:23 +0900

    푸터 공백 수정 및 유저 인포 UI 수정

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -52,7 +52,7 @@ const UserInfo = () => {
         .get("http://192.168.0.203:8080/api/coupons", header)
         .then((res) => res.data.data);
     }
-  );  
+  );
   console.log(coupons);
   const { data: mileage } = useQuery(
     ["mileage", user && user.nickname],
@@ -71,40 +71,42 @@ const UserInfo = () => {
   console.log(mileage);
   return (
     <>
-      <div className="pl-56 w-5/6 min-h-1/2">
-        <p className="text-4xl font-bold text-amber-400  py-14 text-center">
+      <div className="pl-56 w-5/6 min-h-1/2 mb-28">
+        <p className="text-4xl font-bold text-amber-400 py-14 text-center">
           회원정보 변경
         </p>
         <form
           className="flex flex-col border-2 rounded-md p-8 text-center"
           onSubmit={handleUpdate}
         >
-          <label className="w-full font-semibold text-center mb-3">
+          <label className="w-full text-lg font-semibold text-center mb-3">
             비밀번호
           </label>
           <input
-            className="bg-slate-200 rounded shadow-inner text-center h-10 mb-7"
+            className="bg-slate-100 rounded shadow-inner text-center h-10 mb-7"
             name="pwd"
             type="text"
             onChange={handelChange}
           />
-          <label className="w-full font-semibold text-center mb-3">
+          <label className="w-full text-lg font-semibold text-center mb-3">
             닉네임
           </label>
           <input
-            className="bg-slate-200 rounded shadow-inner text-center h-10 mb-7"
+            className="bg-slate-100 rounded shadow-inner text-center h-10 mb-7"
             name="nickname"
             type="text"
             onChange={handelChange}
           />
-          <label className="w-full font-semibold text-center mb-3">주소</label>
+          <label className="w-full text-lg font-semibold text-center mb-3">
+            주소
+          </label>
           <input
-            className="bg-slate-200 rounded shadow-inner text-center h-10 mb-7"
+            className="bg-slate-100 rounded shadow-inner text-center h-10 mb-7"
             name="address"
             type="text"
             onChange={handelChange}
           />
-          <button className="bg-purple-400 w-20 mx-auto text-center rounded text-white">
+          <button className="border-2 border-indigo-400 bg-indigo-400 w-20 mx-auto text-center rounded text-white hover:bg-white hover:text-indigo-400 hover:border-2 ">
             정보수정
           </button>
         </form>
@@ -113,8 +115,8 @@ const UserInfo = () => {
         <p className="text-4xl font-bold text-amber-400  py-14 text-center">
           회원정보
         </p>
-        <div className="flex flex-col border-2 rounded-md p-8 text-center">
-          <p className="w-full font-semibold text-center mb-3">쿠폰</p>
+        <div className="flex flex-col border-2 rounded-md p-8">
+          <p className="w-full text-lg font-bold mb-3">쿠폰&#32;&#32;&#124;</p>
           <ul>
             {coupons &&
               coupons.map((coupon) => (
@@ -123,7 +125,9 @@ const UserInfo = () => {
                 </li>
               ))}
           </ul>
-          <p className="w-full font-semibold text-center mb-3">마일리지</p>
+          <p className="w-full text-lg font-bold my-3">
+            마일리지&#32;&#32;&#124;
+          </p>
           <p>{mileage && mileage[0].mpPrice}</p>
         </div>
       </div>
